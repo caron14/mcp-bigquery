1a04b9641d20c6b55bd2ced6b2aa834e2a7ac9fe
caron14 <supika.caron@gmail.com>
2025-08-23 00:00:12 +0900

    Fix import path for BigQuery client in cache tests

--- tests/test_quality_improvements.py
+++ tests/test_quality_improvements.py
@@ -327,7 +327,7 @@ class TestCache:
         """Test BigQueryClientCache."""
         from mcp_bigquery.cache import BigQueryClientCache
 
-        with patch("mcp_bigquery.cache.get_bigquery_client") as mock_get_client:
+        with patch("mcp_bigquery.bigquery_client.get_bigquery_client") as mock_get_client:
             mock_client = Mock()
             mock_get_client.return_value = mock_client
 
